e1b4ca487c76723996ce60a0ea3b12f72f213356
RebecaTI <mebuffbeka@gmail.com>
2025-04-05 10:40:38 -0300

    feat: integrate recent appointments data into Admin page and enhance StatCard component

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,13 @@
+import DataTable from '@/components/DataTable'
 import StatCard from '@/components/ui/StatCard'
+import { getRecentAppointmentsList } from '@/lib/actions/appointment.actions'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const Admin = () => {
+const Admin = async () => {
+  const appointments = await getRecentAppointmentsList()
+
   return (
     <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
       <header className="admin-header">
@@ -29,23 +33,25 @@ const Admin = () => {
         <section className="admin-stat">
           <StatCard
             type="appointments"
-            count={5}
+            count={appointments.scheduledCount}
             label="Scheduled appointments"
             icon="/assets/icons/appointments.svg"
           />
           <StatCard
             type="pending"
-            count={5}
+            count={appointments.pendingCount}
             label="Pending appointments"
             icon="/assets/icons/pending.svg"
           />
           <StatCard
             type="cancelled"
-            count={5}
+            count={appointments.cancelledCount}
             label="Cancelled appointments"
             icon="/assets/icons/cancelled.svg"
           />
         </section>
+
+        <DataTable column={ } data={appointments.documents} />
 
       </main>
     </div >
---
+++ components/table/columns.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function columns() {
+  return (
+    <div>
+      columns
+    </div>
+  )
+}
+
+export default columns
